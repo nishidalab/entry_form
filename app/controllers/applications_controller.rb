d6f5ced11f91dec7122389aa2f89fd2e294ea74f
okabi <okabi13@yahoo.co.jp>
2017-02-14 21:21:46 +0900

    [ref #6] 実験参加申請フォームの作成(被験者側)
    参加申請ページのビューを作った。

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -15,17 +15,17 @@ class ApplicationsController < ApplicationController
 
   def new
     experiment_id = params['experiment']
-    participant_id = params['participant']
-    if experiment_id.nil? || participant_id.nil?
+    if experiment_id.nil?
       redirect_to applications_url
       return
     end
     @experiment = { id: 1,
                     title: 'ゲーム実験',
                     describe: 'ゲームをする実験です。' }
-    @datetimes = [{ start: DateTime.new(2017, 4, 1, 13, 0), end: DateTime.new(2017, 4, 1, 15, 0) },
-                  { start: DateTime.new(2017, 4, 1, 15, 0), end: DateTime.new(2017, 4, 1, 17, 0) },
-                  { start: DateTime.new(2017, 4, 2, 9, 0), end: DateTime.new(2017, 4, 2, 11, 0) }]
+    @datetimes = [{ start: DateTime.new(2017, 2, 1, 13, 0), end: DateTime.new(2017, 2, 1, 15, 0) },
+                  { start: DateTime.new(2017, 2, 1, 15, 0), end: DateTime.new(2017, 2, 1, 17, 0) },
+                  { start: DateTime.new(2017, 2, 2, 9, 0), end: DateTime.new(2017, 2, 2, 11, 0) }]
+    @participant = { id: 1 }
   end
 
   def create
