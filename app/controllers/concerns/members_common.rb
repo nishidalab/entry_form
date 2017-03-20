efa15c67762e2e9e2be220300f756719ac942f13
yegawa <egawa@ii.ist.i.kyoto-u.ac.jp>
2017-03-20 10:58:01 +0900

    [ref #55] [実験者] 実験募集アクションの作成
    members_controllerと共通で使いそうなメソッドを
    app/controllers/concerns/members_common.rb に追加した

---
+++ app/controllers/concerns/members_common.rb
@@ -0,0 +1,20 @@
+module MembersCommon
+  extend ActiveSupport::Concern
+
+  private
+    # ログインしていない場合メンバーのログインページへリダイレクトする
+    def redirect_to_member_login
+      unless logged_in_member?
+        redirect_to member_login_url
+      end
+    end
+
+    # ログインしていない場合メンバーのログインページへリダイレクトする
+    def redirect_member_logged_in(redirect_path)
+      if logged_in_member?
+        redirect_to redirect_path
+      end
+    end
+
+end
+
