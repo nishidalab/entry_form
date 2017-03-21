6c153192cd45e5a9f432caef5e5ba25e8b4e85d4
yegawa <egawa@ii.ist.i.kyoto-u.ac.jp>
2017-03-21 15:14:20 +0900

    [ref #35] member, experiment, scheduleモデルのテストを書く
    scheduleモデルのテストを書いた。
    app/models/schedule.rbはただのインデント修正です。

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @participant = participants(:one)
+    @experiment = experiments(:one)
+    @schedule = Schedule.new(
+      experiment_id: @experiment.id, participant_id: @participant.id,
+      datetime: DateTime.new(2017,3,23,10,0))
+  end
+
+  test "should be valid" do
+    assert @schedule.valid?
+  end
+
+  test "experiment id should be the id of the experiment exists" do
+    @schedule.experiment_id = Experiment.maximum(:id) + 1
+    assert_not @schedule.valid?
+  end
+
+  test "participant id should be the id of the participant exists" do
+    @schedule.participant_id = Participant.maximum(:id) + 1
+    assert_not @schedule.valid?
+  end
+
 end
