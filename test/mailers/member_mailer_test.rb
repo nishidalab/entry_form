6721f75a204a95bf314b4d0ba51ca475041e79b7
yegawa <egawa@ii.ist.i.kyoto-u.ac.jp>
2017-03-28 15:10:09 +0900

    [ref #71] member登録時に他メンバーの承認が要るようにする
    メールの文面修正＆テスト書いた。

--- test/mailers/member_mailer_test.rb
+++ test/mailers/member_mailer_test.rb
@@ -24,4 +24,49 @@ class MemberMailerTest < ActionMailer::TestCase
       assert_match participant.birth.to_s, body
     end
   end
+
+  test "account_activation" do
+    member = members(:one)
+    member.activation_token = Member.new_token
+    mail = MemberMailer.account_activation(member)
+    assert_equal "アカウント本登録URLについて", mail.subject
+    assert_equal [member.email], mail.to
+    assert_equal ["no-reply@ii.ist.i.kyoto-u.ac.jp"], mail.from
+    assert_match member.name, mail.text_part.body.encoded
+    assert_match member.activation_token, mail.text_part.body.encoded
+    assert_match CGI.escape(member.email), mail.text_part.body.encoded
+    assert_match member.name, mail.html_part.body.encoded
+    assert_match member.activation_token, mail.html_part.body.encoded
+    assert_match CGI.escape(member.email), mail.html_part.body.encoded
+  end
+
+  test "account_activated" do
+    member = members(:one)
+    mail = MemberMailer.account_activated(member)
+    assert_equal "アカウント登録が完了しました", mail.subject
+    assert_equal [member.email], mail.to
+    assert_equal ["no-reply@ii.ist.i.kyoto-u.ac.jp"], mail.from
+    assert_match member.name, mail.text_part.body.encoded
+    recruit_url = 'https://ii.ist.i.kyoto-u.ac.jp/experiment/experiments/new'
+    mypage_url = 'https://ii.ist.i.kyoto-u.ac.jp/experiment/member/mypage'
+    assert_match recruit_url, mail.text_part.body.encoded
+    assert_match mypage_url, mail.text_part.body.encoded
+    assert_match member.name, mail.html_part.body.encoded
+    assert_match recruit_url, mail.html_part.body.encoded
+    assert_match mypage_url, mail.html_part.body.encoded
+  end
+
+  test "password_reset" do
+    member = members(:one)
+    member.reset_token = Member.new_token
+    mail = MemberMailer.password_reset(member)
+    assert_equal "パスワードの再設定", mail.subject
+    assert_equal [member.email], mail.to
+    assert_equal ["no-reply@ii.ist.i.kyoto-u.ac.jp"], mail.from
+    [mail.text_part.body.encoded, mail.html_part.body.encoded].each do |body|
+      assert_match member.name, body
+      assert_match member.reset_token, body
+      assert_match CGI.escape(member.email), body
+    end
+  end
 end
