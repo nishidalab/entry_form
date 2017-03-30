0a276225eabab9ba774fe32a3a43989429a63ba2
silver-1347 <mitsuzumi3yu@gmail.com>
2017-03-30 16:55:28 +0900

    [ref #64] 予定が入っている被験者は退会できないようにする
    participantにdeactivateできるかを判別するメソッドを追加
    削除できないアカウントの場合にdisabledなボタンを表示するように修正
    controllerでアカウント削除して良いかを判定

--- test/integration/participants_deactivate_test.rb
+++ test/integration/participants_deactivate_test.rb
@@ -17,19 +17,19 @@ class ParticipantsDeactivateTest < ActionDispatch::IntegrationTest
   test "get settings and deactivate" do
     log_in_as_participant @participant
     get settings_path
-    if @participant.schedules.blank? && @participant.events.blank?
+    if @participant.deactivatable?
       assert_select "a[href=?]", deactivate_path
+      deactivate
+      assert_redirected_to login_url
+      follow_redirect!
+      assert_not flash.empty?
+      assert_not is_logged_in_participant?
+      @participant.reload
+      assert @participant.deactivated
+      get login_path
+      assert flash.empty?
     else
       assert_select "a[href=?]", deactivate_path, false
     end
-    deactivate
-    assert_redirected_to login_url
-    follow_redirect!
-    assert_not flash.empty?
-    assert_not is_logged_in_participant?
-    @participant.reload
-    assert @participant.deactivated
-    get login_path
-    assert flash.empty?
   end
 end
