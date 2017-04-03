d41add3dde507a9a70f97788b895263d680bd049
yegawa <egawa@ii.ist.i.kyoto-u.ac.jp>
2017-04-03 20:33:23 +0900

    [ref #92] [member] マイページに実験予定を表示する
    1つのscheduleに複数人いた場合の処理書いた。
    癖でjsだよりなコードになってしまった。

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -27,18 +27,24 @@ class MembersController < ApplicationController
 
     @times = []
     @schedules.each do |s|
-      participant = ''
+      p_infos = []
       my_apps = Application.where(schedule_id: s.id).where(status: [0, 1])
       if !my_apps.empty?
-        participant = Participant.find_by_id(my_apps[0].participant_id).name
-      else
-        participant = '空き'
+        my_apps.each do |my_app|
+          p_info = {}
+          p = Participant.find_by_id(my_app.participant_id)
+          p_info[:id] = p.id
+          p_info[:name] = p.name
+          p_info[:yomi] = p.yomi
+          p_info[:status] = my_app.status
+          p_infos.push(p_info)
+        end
       end
       @times.push({
         start: s.datetime,
         end: s.datetime + s.experiment.duration * 60,
         experiment: @experiments.find_by_id(s.experiment_id).name,
-        participant: participant})
+        p_infos: p_infos})
     end
   end
 
