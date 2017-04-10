66199a77752db2f2d3acf4c3217167d3302ddd4b
silver-1347 <mitsuzumi3yu@gmail.com>
2017-04-11 00:12:51 +0900

    [ref #46] メールアドレスを更新できるようにする(被験者)
    participantにchanging_emailを追加

--- db/migrate/20170408160354_add_email_updates_to_participants.rb
+++ db/migrate/20170408160354_add_email_updates_to_participants.rb
@@ -2,5 +2,6 @@ class AddEmailUpdatesToParticipants < ActiveRecord::Migration[5.0]
   def change
     add_column :participants, :email_update_token, :string
     add_column :participants, :new_email, :string
+    add_column :participants, :changing_email, :boolean, default: false
   end
 end
